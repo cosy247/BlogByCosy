ae77ccd942d161bf6ac2a5140c3199f9bfb8de34
cosy247 <705194673@qq.com>
2024-11-29 17:05:59 +0800

    🎉 init: 剔除

---
+++ src/components/PageOuter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="PageOuter">
+    <div class="menu">MENU</div>
+    <div class="logo">COSY</div>
+    <div class="links"></div>
+    <div class="infos"></div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.PageOuter {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
+<template>
+    <div class='HomeView'>
+        
+    </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.HomeView{
+    
+}
+</style>
