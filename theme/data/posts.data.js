78826531aab432820155dbd281a7259634a46778
cosy247 <705194673@qq.com>
2025-05-22 16:38:08 +0800

    🐞fix: 在数据转换中添加过滤条件，确保隐藏的文章不被包含

--- theme/data/posts.data.js
+++ theme/data/posts.data.js
@@ -15,7 +15,9 @@ export default createContentLoader(`../${config.srcDir}/**.md`, {
   includeSrc: false,
   excerpt: false,
   async transform(data) {
-    const posts = JSON.parse(JSON.stringify(data)).filter((d) => d.frontmatter.id && !d.url.startsWith('/@'));
+    const posts = JSON.parse(JSON.stringify(data)).filter(
+      (d) => d.frontmatter.id && !d.url.startsWith('/@') && d.frontmatter.hidden !== true
+    );
     posts.forEach((post) => (post.url = `${config.base}${post.url.slice(1)}`));
     posts.forEach((post) => {
       // 处理文章的 classify
