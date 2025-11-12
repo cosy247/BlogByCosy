b052a8b0dac01664ad8954f12815c1f441eb4a94
cosy247 <705194673@qq.com>
2025-11-12 16:16:41 +0800

    🐞fix: 更新脚本用法说明，添加对 'dev' 命令的支持

--- docs.bat
+++ docs.bat
@@ -6,14 +6,17 @@ if "%1"=="list" (
     npm run list
 ) else if "%1"=="new" (
     npm run new
+) else if "%1"=="dev" (
+    npm run dev
 ) else if "%1"=="file" (
     explorer.exe . 
 ) else if "%1"=="cmd" ( 
     cmd.exe /k cd "%DIR%" 
 ) else (
-    echo Usage: docs [list ^| new ^| file ^| cmd]
-    echo   list - Execute npm run list
-    echo   new  - Execute npm run new
-    echo   file - Open file explorer in current directory
-    echo   cmd  - Open CMD window in current directory
+    echo Usage: docs [list ^| new ^| file ^| dev ^| cmd]
+    echo   list - Display article list.
+    echo   new  - Create a new article.
+    echo   dev  - Start article server.
+    echo   file - Open file explorer in current directory.
+    echo   cmd  - Open CMD window in current directory.
 )
